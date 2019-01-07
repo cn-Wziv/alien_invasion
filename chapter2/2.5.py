df42919cae6510a9325cb121acfc67dbec4e1cae
ziv <ziv_YNote@163.com>
2019-01-07 12:32:53 +0800

    chapter2_1

---
+++ chapter2/2.5.py
@@ -0,0 +1,32 @@
+'''
+编写注释的主要目的是阐述代码要做什么，以及是如何做的。在开发项目期间，你对各个部分如何协同工作了如指掌，但过段时间后，有些细节你可能不记得了。当然，你总是
+可以通过研究代码来确定各个部分的工作原理，但通过编写注释，以清晰的自然语言对解决方案进行概述，可节省很多时间。
+要成为专业程序员或与其他程序员合作，就必须编写有意义的注释。当前，大多数软件都是合作编写的，编写者可能是同一家公司的多名员工，也可能是众多致力于同一个开源
+项目的人员。训练有素的程序员都希望代码中包含注释，因此你最好从现在开始就在程序中添加描述性注释。作为新手，最值得养成的习惯之一是，在代码中编写清晰、简洁的
+注释。
+如果不确定是否要编写注释，就问问自己，找到合理的解决方案前，是否考虑了多个解决方案。如果答案是肯定的，就编写注释对你的解决方案进行说明吧。相比回过头去再添
+加注释，删除多余的注释要容易得多。从现在开始，本书的示例都将使用注释来阐述代码的工作原理
+'''
+
+'''
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.                                          漂亮总好过丑陋
+Explicit is better than implicit.                                       明确总好过含糊
+Complex is better than complicated.                                     复杂总好过混乱
+Flat is better than nested.                                             顺序总好过嵌套    
+Sparse is better than dense.                                            稀疏总好过密集
+Readability counts.                                                     可读性很重要   
+Special cases aren't special enough to break the rules.                 特殊情况不足以打破规则
+Although practicality beats purity.                                     实用性第一
+Errors should never pass silently.                                      错误不该被默默地通过
+Unless explicitly silenced.                                             除非明确沉默
+In the face of ambiguity, refuse the temptation to guess.               面对歧义，不轻易通过猜测下结论
+There should be one-- and preferably only one --obvious way to do it.   应该有且最好只有一种显而易见的方式来解决问题
+Although that way may not be obvious at first unless you're Dutch.      尽管这种方式可能一开始并不明显
+Now is better than never.                                               现在总比没有好
+Although never is often better than *right* now.                        尽管从来没有比现在更好
+If the implementation is hard to explain, it's a bad idea.              如果实现方式很难解释，那它并不是一个好的方案
+If the implementation is easy to explain, it may be a good idea.        如果实现方式容易解释，那它可能是是一个好的方案
+Namespaces are one honking great idea -- let's do more of those!        命名空间是一个好的想法，让我们做更多的事吧
+'''
