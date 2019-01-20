c54ca7d153461816f4cb05167dca31c069b8a100
ziv <ziv_YNote@163.com>
2019-01-20 22:07:55 +0800

    alien_invasion 2

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,7 +3,8 @@ from setting import Setting
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
-
+from alien import Alien
+from game_stats import GameStats
 def run_game():
 
     # 初始化游戏并创建一个屏幕对象
@@ -13,11 +14,17 @@ def run_game():
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
-
+    # 创建用于存储有效外星人的编组
+    aliens = Group()
     # 创建用于存储有效子弹的编组
     bullets = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         '''
@@ -26,12 +33,16 @@ def run_game():
         '''
         # 监视键盘和鼠标
         gf.check_events(ai_settings, screen, ship, bullets)
-        # 飞船移动
-        ship.update()
-        # 子弹绘出和删除
-        gf.update_bullet(bullets)
+        # 游戏状态为True时运行游戏
+        if stats.game_active:
+            # 飞船移动
+            ship.update()
+            # 子弹绘出和删除
+            gf.update_bullet(ai_settings, screen, ship, bullets, aliens)
+            # 外星人的移动
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens,bullets)
         # 设置背景颜色，让最近绘制的屏幕可见
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- alien_invasion/setting.py
+++ alien_invasion/setting.py
@@ -3,14 +3,27 @@ class Setting():
     用于存储游戏中用到的各种设置
     '''
     def __init__(self):
+        # 背景
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
-        self.ship_speed_factor = 1.5
+
+        # 飞船相关
+        self.ship_speed_factor = 1.5        # 速度
+        self.ship_limit = 3                 # 命
+
+        # 子弹相关
         # 子弹设置
         self.bullet_speed_factor = 3
         self.bullet_width = 3
-        self.bullet_height = 10
+        self.bullet_height = 5
         self.bullet_color = 60, 60, 60
         # 屏幕上出现的最多子弹数
         self.bullet_allowed = 3
+
+        # 外星人相关
+        # 外星人移动速度
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 50
+        # fleet_direction为1表示向右移，-1表示向左移
+        self.fleet_direction = 1
